503522109355c3219a70c2df49b7575ae7e1a775
Davin Huckleberry <dhuck@tinycact.us>
2019-11-06 20:27:27 -0600

    more specification

---
+++ points/__init__.py
@@ -0,0 +1,9 @@
+from .build import *
+from .display import *
+from .input import *
+from .letters import *
+
+if __name__ = '__main__':
+    pass
+else:
+    pass
---
+++ points/display.py
@@ -0,0 +1,18 @@
+def sixToBraille(word):
+    """
+    Takes a word (6 characters max) and converts it to the output array
+    for sendDisplay()
+
+    :param word: 6 character string
+    :return list: a multidimensional list of bytes
+    """
+    pass
+
+def sendDisplay(rows):
+    """
+    Sends the rows to the binary file to be displayed on the 8x8 matrix.
+
+    :param rows: multimensional list of bytes
+    :return none:
+    """
+    pass
