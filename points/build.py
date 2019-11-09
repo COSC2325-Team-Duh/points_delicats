c0788a2e7530d32b919ba7a19d6de9fcd74a9083
dhuck <dhuck@tinycact.us>
2019-11-09 10:41:46 -0600

    created python build system

--- points/build.py
+++ points/build.py
@@ -1,14 +1,42 @@
+import os
+import logging
+BINARY = "./points.bin"
+logging.basicConfig(level=logging.DEBUG)
+
 def buildAssembly():
     """
     This function will run a make command and verify that a binary file
     was created as a result of the make command.
+
+    :return bool: Return True if built, False if failure
     """
-    pass
+    if checkAssembly(BINARY):
+        logging.debug("Binary exists, buildAssembly() has nothing to do...")
+        return True
+    else:
+        logging.debug("Binary does not exist, building...")
+        make = os.popen("make").read()
+        print(make)
+        if checkAssembly(BINARY):
+            logging.debug("Binary Built")
+            return True
+        else:
+            logging.debug("Something went wrong... :(")
+            return False
+
+
 
 def checkAssembly(file):
     """
     This function checks to see if the supplied file exists or not.
 
     :param file: filename of the binary
+    :return bool: returns True if binary exists, False if doesn't
     """
+    logging.debug("Checking if binary exists")
+    return os.path.exists(file)
+
+if __name__ == "__main__":
+    buildAssembly()
+else:
     pass
