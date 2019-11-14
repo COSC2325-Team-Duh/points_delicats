916a78d9a46ea464d4ccc14d3d78d5a278acd318
akinser42 <56604342+akinser42@users.noreply.github.com>
2019-11-13 19:12:21 -0600

    made formatInput

--- points/input.py
+++ points/input.py
@@ -7,3 +7,19 @@ def formatInput(text):
     :return text: string of text
     """
     pass
+
+    inputString = input("Input a string: ")
+
+    newString = ""
+
+    for index in inputString:
+            if index == " ":
+                newString += index
+            elif index >= "a" and index <= "z":
+                newString += index
+            elif index >= "A" and index <= "Z":
+                newString += index.lower()
+
+    print(newString)
+
+
