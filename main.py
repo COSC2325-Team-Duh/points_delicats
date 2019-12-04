96f0d4caad56032627e12b8601c38629fed4e2d4
dhuck <davin@tinycact.us>
2019-12-03 23:20:49 -0600

    cleaned up main

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
+# set up logging library
 import logging
+logging.basicConfig(format='%(asctime)s .:%(levelname)s %(message)s', level=logging.DEBUG)
+
 import points as p
 from points import NoBinaryException
 from time import sleep
 
-# set up logging library
-logging.basicConfig(format='%(asctime)s .:%(levelname)s %(message)s', level=logging.DEBUG)
-
-def setup():
-    if p.buildAssembly() == False:
-        raise
-
 def main():
-    phrase = input("Phrase: ")
     logging.info("Enter a phrase! -1 will exit the program")
+    phrase = input("Phrase: ")
     while phrase != "-1":
         phrase = p.formatInput(phrase[0:6])
         logging.debug("The phrase is: {}".format(phrase))
@@ -25,10 +21,8 @@ def main():
 
 if __name__ == "__main__":
     try:
-        setup()
-    except NoBinaryException as e:
-        logging.critical("ERROR: {}".format(e))
-
-    main()
+        main()
+    except KeyboardInterrupt:
+        logging.info("Goodbye!")
 else:
     pass
--- points/__init__.py
+++ points/__init__.py
@@ -2,8 +2,12 @@ from .build import *
 from .display import *
 from .input import *
 from .letters import *
-from set_pin import *
-
+try:
+    from set_pin_mode import *
+    from pin_write import *
+except ModuleNotFoundError:
+    logging.critical("GPIO Library not found! please run setup.py install")
+    exit()
 if __name__ == '__main__':
     pass
 else:
