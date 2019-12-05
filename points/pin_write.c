3773b2407e1d844b3aa875c41a9becd98137cdce
dhuck <davin@tinycact.us>
2019-12-05 03:01:17 -0600

    simplified C extension into onefile

--- points/pin_write.c
+++ points/pin_write.c
@@ -1,26 +1,51 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static PyObject *method_pin_write(PyObject *self, PyObject *args) {
-    int *pin, value;
-    extern void gpioSet(int*);
-    extern void gpioClr(int*);
+uint32_t *gpio_memory;
 
+static PyObject *method_pin_write(PyObject *self, PyObject *args) {
+    int pin, value;
+    extern void gpioSet(uint32_t*, int);
+    extern void gpioClr(uint32_t*, int);
     if (!PyArg_ParseTuple(args, "ii", &pin, &value)) {
         Py_INCREF(Py_None);
+        return Py_None;
     }
     if (value == 1) {
-        gpioSet(pin);
+        gpioSet(gpio_memory, pin);
     }
     else if (value == 0) {
-        gpioClr(pin);
+        gpioClr(gpio_memory, pin);
     }
     Py_INCREF(Py_None);
     return Py_None;
 }
 
+static PyObject *method_set_pin_mode(PyObject *self, PyObject *args) {
+    int *pin, *mode;
+    extern void gpioSelect(int*, int*);
+
+    if (!PyArg_ParseTuple(args, "ii", &pin, &mode)) {
+        Py_INCREF(Py_None);
+        return Py_None;
+    }
+    gpioSelect(pin, mode);
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject * method_setup_pins(PyObject *self, PyObject *args) {
+    extern uint32_t* mapMem();
+    gpio_memory = mapMem();
+    printf("GPIO Loc: %p\n", gpio_memory);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
 static PyMethodDef Pin_WriteMethods[] = {
     {"pin_write", method_pin_write, METH_VARARGS, "Writes to GPIO pin"},
+    {"set_pin_mode", method_set_pin_mode, METH_VARARGS, "Assembly GPIO interface"},
+    {"setup_pins", method_setup_pins, METH_VARARGS, "MAPS the GPIO address and sets up library"},
     {NULL, NULL, 0, NULL}
 };
 
@@ -28,7 +53,7 @@ static struct PyModuleDef pin_writemodule = {
     PyModuleDef_HEAD_INIT,
     "pin_write",
     "Writes to GPIO pin",
-    0,
+    -1,
     Pin_WriteMethods,
 };
 
